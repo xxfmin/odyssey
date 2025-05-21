8aa47be955e3e478e89937d3600dc531a175850e
Felipe Min <felipemin03@gmail.com>
2025-05-21 16:19:55 -0400

    added cursor-pointer to buttons

--- app/components/dashNavbar.tsx
+++ app/components/dashNavbar.tsx
@@ -22,7 +22,7 @@ export const DashNavbar = () => {
 
   return (
     <nav
-      className={`fixed top-0 left-0 right-0 z-50 py-2 px-4 flex items-center justify-between bg-white border-b border-gray-300 ${poppins.className}`}
+      className={`fixed top-0 left-0 right-0 z-50 py-2 px-4 flex items-center justify-between bg-white border-b border-gray-300 cursor-pointer ${poppins.className}`}
     >
       <Link href="/dashboard" className="text-3xl font-bold text-blue-500">
         odyssey
@@ -30,13 +30,13 @@ export const DashNavbar = () => {
       <div>
         <Link
           href="/dashboard"
-          className="px-4 py-2 rounded-full hover:bg-gray-200 transition font-bold text-md"
+          className="px-4 py-2 rounded-full hover:bg-gray-200 transition font-bold text-md cursor-pointer"
         >
           Home
         </Link>
         <button
           onClick={handleLogout}
-          className="px-4 py-2 rounded-full hover:bg-gray-200 transition font-bold text-md"
+          className="px-4 py-2 rounded-full hover:bg-gray-200 transition font-bold text-md cursor-pointer"
         >
           Sign out
         </button>
